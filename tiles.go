74351f13760f201706cd5812a72302ef0a2c6ffa
Chris Lefever <linuxcl4@gmail.com>
2022-05-12 14:26:28 -0400

    Initial commit

---
+++ tiles.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"fmt"
+
+	svg "github.com/ajstarks/svgo"
+)
+
+type TileSize int
+
+const (
+	Small  TileSize = 60
+	Medium TileSize = 120
+	Large  TileSize = 240
+)
+
+type Tile struct {
+	X    int
+	Y    int
+	Size TileSize
+}
+
+func DrawBase(canvas *svg.SVG, x int, y int, size int, invert bool) {
+	var color string
+	if invert {
+		color = "black"
+	} else {
+		color = "white"
+	}
+	style := fmt.Sprintf("fill:%s", color)
+	canvas.Circle(x+size/4, y+size/4, size/6, style)
+	canvas.Circle(x+size-size/4, y+size/4, size/6, style)
+	canvas.Circle(x+size/4, y+size-size/4, size/6, style)
+	canvas.Circle(x+size-size/4, y+size-size/4, size/6, style)
+	canvas.Circle(x+size/2, y+size/2, size/4, style)
+}
+
+func DrawDots(canvas *svg.SVG, x int, y int, size int, invert bool) {
+	var color string
+	if invert {
+		color = "white"
+	} else {
+		color = "black"
+	}
+	style := fmt.Sprintf("fill:%s", color)
+	canvas.Circle(x+size/2, y+size/4, size/12, style)
+	canvas.Circle(x+size/4, y+size/2, size/12, style)
+	canvas.Circle(x+size-size/4, y+size/2, size/12, style)
+	canvas.Circle(x+size/2, y+size-size/4, size/12, style)
+}
+
+func Tile1(canvas *svg.SVG, x int, y int, size int, invert bool) {
+	DrawBase(canvas, x, y, size, invert)
+	var color string
+	if invert {
+		color = "white"
+	} else {
+		color = "black"
+	}
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", color, size/6)
+	canvas.Arc(x+size/2, y+size/4, size/4, size/4, 0, false, false, x+size-size/4, y+size/2, style)
+	canvas.Arc(x+size/2, y+size-size/4, size/4, size/4, 0, false, false, x+size/4, y+size/2, style)
+	DrawDots(canvas, x, y, size, invert)
+}
+
+func Tile2(canvas *svg.SVG, x int, y int, size int, invert bool) {
+	DrawBase(canvas, x, y, size, invert)
+	var color string
+	if invert {
+		color = "white"
+	} else {
+		color = "black"
+	}
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", color, size/6)
+	canvas.Arc(x+size/2, y+size/4, size/4, size/4, 0, false, true, x+size/4, y+size/2, style)
+	canvas.Arc(x+size/2, y+size-size/4, size/4, size/4, 0, false, true, x+size-size/4, y+size/2, style)
+	DrawDots(canvas, x, y, size, invert)
+}
+
+func Tile3(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", "black", size/6)
+	canvas.Line(x+size/4, y+size/2, x+size-size/4, y+size/2, style)
+	DrawDots(canvas, x, y, size, false)
+}
+
+func Tile4(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", "black", size/6)
+	canvas.Line(x+size/2, y+size/4, x+size/2, y+size-size/4, style)
+	DrawDots(canvas, x, y, size, false)
+}
+
+func Tile5(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	DrawDots(canvas, x, y, size, false)
+}
+
+func Tile6(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", "black", size/6)
+	canvas.Arc(x+size/2, y+size/4, size/4, size/4, 0, false, false, x+size-size/4, y+size/2, style)
+	canvas.Arc(x+size/2, y+size-size/4, size/4, size/4, 0, false, false, x+size/4, y+size/2, style)
+	canvas.Arc(x+size/2, y+size/4, size/4, size/4, 0, false, true, x+size/4, y+size/2, style)
+	canvas.Arc(x+size/2, y+size-size/4, size/4, size/4, 0, false, true, x+size-size/4, y+size/2, style)
+	canvas.Circle(x+size/2, y+size/2, size/12, "fill:black")
+	DrawDots(canvas, x, y, size, false)
+}
+
+func Tile7(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", "black", size/6)
+	canvas.Line(x+size/4, y+size/2, x+size-size/4, y+size/2, style)
+	canvas.Line(x+size/2, y+size/4, x+size/2, y+size-size/4, style)
+	DrawDots(canvas, x, y, size, false)
+}
+
+func Tile8(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", "black", size/6)
+	canvas.Arc(x+size/2, y+size/4, size/4, size/4, 0, false, false, x+size-size/4, y+size/2, style)
+	DrawDots(canvas, x, y, size, false)
+}
+
+func Tile9(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", "black", size/6)
+	canvas.Arc(x+size/2, y+size-size/4, size/4, size/4, 0, false, false, x+size/4, y+size/2, style)
+	DrawDots(canvas, x, y, size, false)
+}
+
+func Tile10(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", "black", size/6)
+	canvas.Arc(x+size/2, y+size/4, size/4, size/4, 0, false, true, x+size/4, y+size/2, style)
+	DrawDots(canvas, x, y, size, false)
+}
+
+func Tile11(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", "black", size/6)
+	canvas.Arc(x+size/2, y+size-size/4, size/4, size/4, 0, false, true, x+size-size/4, y+size/2, style)
+	DrawDots(canvas, x, y, size, false)
+}
+
+func Tile12(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", "black", size/6)
+	canvas.Arc(x+size/2, y+size/4, size/4, size/4, 0, false, false, x+size-size/4, y+size/2, style)
+	canvas.Arc(x+size/2, y+size/4, size/4, size/4, 0, false, true, x+size/4, y+size/2, style)
+	canvas.Line(x+size/4, y+size/2, x+size-size/4, y+size/2, style)
+	DrawDots(canvas, x, y, size, false)
+}
+
+func Tile13(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", "black", size/6)
+	canvas.Arc(x+size/2, y+size-size/4, size/4, size/4, 0, false, true, x+size-size/4, y+size/2, style)
+	DrawDots(canvas, x, y, size, false)
+}
+
+func Tile14(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", "black", size/6)
+	canvas.Arc(x+size/2, y+size-size/4, size/4, size/4, 0, false, true, x+size-size/4, y+size/2, style)
+	DrawDots(canvas, x, y, size, false)
+}
+
+func Tile15(canvas *svg.SVG, x int, y int, size int) {
+	DrawBase(canvas, x, y, size, false)
+	style := fmt.Sprintf("fill:%s;stroke:%s;stroke-width:%d", "none", "black", size/6)
+	canvas.Arc(x+size/2, y+size-size/4, size/4, size/4, 0, false, true, x+size-size/4, y+size/2, style)
+	DrawDots(canvas, x, y, size, false)
+}
